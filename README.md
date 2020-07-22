2eaa7ccec2c6a8b9399315abc567c6ca24dbe7bb
MartkCz <martk494@gmail.com>
2020-07-22 12:09:10 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## doctrine utility
+
+[![Build Status](https://travis-ci.org/utilitte/doctrine.svg?branch=master)](https://travis-ci.org/utilitte/doctrine)
+[![Licence](https://img.shields.io/packagist/l/utilitte/doctrine.svg)](https://packagist.org/packages/utilitte/doctrine)
+[![Latest stable](https://img.shields.io/packagist/v/utilitte/doctrine.svg)](https://packagist.org/packages/utilitte/doctrine)
---
+++ src/DoctrineIdentityExtractor.php
@@ -0,0 +1,60 @@
+<?php declare(strict_types = 1);
+
+namespace Utilitte\Doctrine;
+
+use Doctrine\ORM\EntityManagerInterface;
+use InvalidArgumentException;
+
+final class DoctrineIdentityExtractor
+{
+
+	private EntityManagerInterface $em;
+
+	public function __construct(EntityManagerInterface $em)
+	{
+		$this->em = $em;
+	}
+
+	/**
+	 * @param object[] $entities
+	 * @return mixed[]
+	 */
+	public function extractIdentities(iterable $entities, bool $allowMixing = false): array
+	{
+		$type = null;
+		$ids = [];
+
+		foreach ($entities as $entity) {
+			if (!is_object($entity)) {
+				throw new InvalidArgumentException(
+					sprintf('Given array must be an array of object, %s contained in array', gettype($entity))
+				);
+			}
+
+			$className = get_class($entity);
+
+			if (!$allowMixing) {
+				$this->checkType($className, $type);
+			}
+
+			$values = $this->em->getClassMetadata($className)->getIdentifierValues($entity);
+			$ids[] = current($values);
+		}
+
+		return $ids;
+	}
+
+	private function checkType(string $class, ?string $type): void
+	{
+		if (!$type) {
+			return;
+		}
+
+		if ($type !== $class) {
+			throw new InvalidArgumentException(
+				sprintf('Given array must be an array of %s, %s contained in array', $type, $class)
+			);
+		}
+	}
+
+}
