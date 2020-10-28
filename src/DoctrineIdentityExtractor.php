c40e448d376eb357e908d04c609aa56a073c1923
MartkCz <martk494@gmail.com>
2020-10-28 10:50:42 +0100

    add extractIdentityMany, refactor

--- src/DoctrineIdentityExtractor.php
+++ src/DoctrineIdentityExtractor.php
@@ -4,6 +4,7 @@ namespace Utilitte\Doctrine;
 
 use Doctrine\ORM\EntityManagerInterface;
 use InvalidArgumentException;
+use LogicException;
 
 final class DoctrineIdentityExtractor
 {
@@ -16,10 +17,31 @@ final class DoctrineIdentityExtractor
 	}
 
 	/**
+	 * @return mixed[]
+	 */
+	public function extractIdentities(object $entity): array
+	{
+		return $this->em->getClassMetadata(get_class($entity))->getIdentifierValues($entity);
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function extractIdentity(object $entity)
+	{
+		$ids = $this->extractIdentities($entity);
+		if (count($ids) !== 1) {
+			throw new InvalidArgumentException(sprintf('Entity "%s" must have one identity', get_class($entity)));
+		}
+
+		return current($ids);
+	}
+
+	/**
 	 * @param object[] $entities
 	 * @return mixed[]
 	 */
-	public function extractIdentities(iterable $entities, bool $allowTypeMixing = false): array
+	public function extractIdentitiesMany(iterable $entities, bool $allowMixing = false): array
 	{
 		$type = null;
 		$ids = [];
@@ -32,24 +54,60 @@ final class DoctrineIdentityExtractor
 			}
 
 			$className = get_class($entity);
-
-			if (!$allowTypeMixing) {
-				$this->checkType($className, $type);
+			if (!$className) {
+				throw new LogicException('Cannot get class name');
 			}
 
-			$values = $this->em->getClassMetadata($className)->getIdentifierValues($entity);
-			$ids[] = current($values);
+			if (!$allowTypeMixing) {
+				if (!$type) {
+					$type = $className;
+				} else {
+					$this->checkType($className, $type);
+				}
+			}
+
+			$ids[] = $this->extractIdentities($entity);
 		}
 
 		return $ids;
 	}
 
-	private function checkType(string $class, ?string $type): void
+	/**
+	 * @return mixed
+	 */
+	public function extractIdentityMany(iterable $entities, bool $allowTypeMixing = false)
 	{
-		if (!$type) {
-			return;
+		$type = null;
+		$ids = [];
+
+		foreach ($entities as $entity) {
+			if (!is_object($entity)) {
+				throw new InvalidArgumentException(
+					sprintf('Given array must be an array of object, %s contained in array', gettype($entity))
+				);
+			}
+
+			$className = get_class($entity);
+			if (!$className) {
+				throw new LogicException('Cannot get class name');
+			}
+
+			if (!$allowTypeMixing) {
+				if (!$type) {
+					$type = $className;
+				} else {
+					$this->checkType($className, $type);
+				}
+			}
+
+			$ids[] = $this->extractIdentity($entity);
 		}
 
+		return $ids;
+	}
+
+	private function checkType(string $class, string $type): void
+	{
 		if ($type !== $class) {
 			throw new InvalidArgumentException(
 				sprintf('Given array must be an array of %s, %s contained in array', $type, $class)
